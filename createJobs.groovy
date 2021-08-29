e1cfd0a41d9be30000a6f804d191b2bb5337d543
roxana.ghenea <roxana.ghenea@gmail.com>
2021-08-29 20:33:51 +0300

    updated createJobs groovy script

--- createJobs.groovy
+++ createJobs.groovy
@@ -1,8 +1,14 @@
-pipelineJob('pipelineJob') {
+pipelineJob('theme-park-job') {
     definition {
-        cps {
-            script(readFileFromWorkspace('pipelineJob.groovy'))
-            sandbox()
+        cpsScm {
+            scm {
+                git {
+                    remote {
+                        url 'https://github.com/sprinkle24/ThemePark.git'
+                    }
+                    branch 'master'
+                }
+            }
         }
     }
 }
